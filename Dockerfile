77689c0a3a040eb35d114aace22c36fed8c81c36
nirsht <nir@komodor.io>
2022-06-28 23:34:27 +0300

    Add docker-comspoer

---
+++ Dockerfile
@@ -0,0 +1,5 @@
+FROM node:16.15.1
+
+RUN npm install
+
+WORKDIR /var/www/app
---
+++ server.js
@@ -0,0 +1,62 @@
+const multer = require("multer");
+const csv = require("fast-csv");
+const fs = require("fs");
+const express = require("express");
+const redis = require("redis");
+const { promisify } = require("util");
+
+const redisClient = redis.createClient(process.env.REDIS_URL);
+
+const app = express();
+const PORT = process.env.PORT || 5001;
+
+global.__basedir = __dirname;
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, __basedir + "/uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + "-" + Date.now() + "-" + file.originalname);
+  },
+});
+
+const csvFilter = (req, file, cb) => {
+  if (file.mimetype.includes("csv")) {
+    cb(null, true);
+  } else {
+    cb("Please upload csv only.", false);
+  }
+};
+
+const upload = multer({ storage: storage, fileFilter: csvFilter });
+
+app.get("/", function (req, res) {
+  res.send("GET request to homepage");
+});
+
+app.post("/players", upload.single("file"), (req, res) => {
+  if (req.file == undefined) {
+    return res.status(400).send({ message: "Please upload csv file." });
+  }
+
+  let csvData = [];
+  let filePath = __basedir + "/uploads/" + req.file.filename;
+  fs.createReadStream(filePath)
+    .pipe(csv.parse({ headers: true }))
+    .on("error", (error) => {
+      throw error.message;
+    })
+    .on("data", (row) => {
+      csvData.push(row);
+    })
+    .on("end", () => {
+      console.log("csvData");
+      console.log(csvData);
+      res.send(200);
+    });
+});
+
+app.listen(PORT, () => {
+  console.log(`Now listening on port ${PORT}`);
+});
